8efb5e7dce80cafb5e98e1dcc8b6a8a623eb07ac
ElsaStromback <elsastrmbck@gmail.com>
2023-12-09 16:21:52 +0100

    Lagt till emotional state i respons_dictionary

--- interaction/script.py
+++ interaction/script.py
@@ -3,7 +3,7 @@ from furhat_remote_api import FurhatRemoteAPI
 import dictionary as dict
 import re
 import random
-FURHAT_IP = "172.20.10.2"
+FURHAT_IP = "192.168.1.125"
 
 furhat = FurhatRemoteAPI(FURHAT_IP)
 furhat.set_led(red=100, green=50, blue=50)
@@ -60,19 +60,16 @@ def suggest_drink(emotion, answer):
         bsay("Mabye a Long island ice tea?")
     if "fresh" or "refreshing" in answer.message:
         bsay("Mabye a water?")
+    
+def handle_emotional_state(): ##Detta ska kombineras me 
+    set_gesture("BigSmile")
+    return "positive"
+    
 
-
-def demo_bartender():
-    set_persona('Marty')
-    responses_dict = dict.create_responses()
-    keywords_dict = dict.create_keywords_dict()
-
-    print("hej")
-    while(True):
-        response = furhat.listen() 
-
+def generate_response(responses_dict,keywords_dict,response,customer_feeling):
         print(response)
         matched_intent = None
+
         for intent,pattern in keywords_dict.items():
         # Using the regular expression search function to look for keywords in user input
             if re.search(pattern, response.message): 
@@ -82,11 +79,30 @@ def demo_bartender():
         key='fallback' 
         if matched_intent in responses_dict:
             key = matched_intent
-            bsay(responses_dict[key])
+            if customer_feeling == 'positive' and 'positive_responses' in responses_dict[key]:
+                bsay(random.choice(responses_dict[key]['positive_responses']))
+            elif customer_feeling == 'negative' and 'negative_responses' in responses_dict[key]:
+                bsay(random.choice(responses_dict[key]['negative_responses']))
+            elif customer_feeling == 'natural' and 'natural_responses' in responses_dict[key]:
+                bsay(random.choice(responses_dict[key]['natural_responses']))
         else:
             set_gesture("Oh")
             set_gesture("BrowRaise")
-            bsay(random.choice(responses_dict[key]))
+            bsay(random.choice(responses_dict[key].get('fallback_responses', []))) 
+
+
+def demo_bartender():
+    set_persona('Marty')
+    responses_dict = dict.create_responses()
+    keywords_dict = dict.create_keywords_dict()
+
+    bsay("hej")
+    while(True):
+        response = furhat.listen() 
+        costumer_feeling = handle_emotional_state() 
+        generate_response(responses_dict,keywords_dict,response,costumer_feeling)
+
+
 
 if __name__ == '__main__':
     demo_bartender()
